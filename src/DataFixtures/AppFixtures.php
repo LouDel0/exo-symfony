e48d4f2c3421aadf3072974ad7043111e367911d
Louison <louison.delagarde@gmail.com>
2023-01-31 16:23:03 +0100

    Test twig

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,28 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $encoder;
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $user = new User();
+        $user->setEmail('moche@email.com');
+        $password = $this->encoder->hashPassword($user, "sdfzsdfs");
+        $user->setPassword($password);
+
+        $manager->persist($user);
+
         // $product = new Product();
         // $manager->persist($product);
 
