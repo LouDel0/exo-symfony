8ae347a7e867bf06ef57573d32f13e33a77593fb
Louison <louison.delagarde@gmail.com>
2023-02-08 11:07:40 +0100

    trucs

--- src/Form/Product1Type.php
+++ src/Form/Product1Type.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Product;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class Product1Type extends AbstractType
 {
@@ -15,10 +18,21 @@ class Product1Type extends AbstractType
             ->add('name')
             ->add('description')
             ->add('price')
-            ->add('datetime')
             ->add('image')
-            ->add('category_id')
-        ;
+            ->add(
+                'category_id',
+                EntityType::class,
+                [
+                    'class' => Category::class,
+                    'choice_label' => 'name',
+                    'placeholder' => 'Choisir une catégorie',
+                    'label' => 'Catégorie',
+                ]
+            )
+            ->add('datetime', DateType::class, array(
+                'format' => 'dd-MM-yyyy',
+                'data' => new \DateTime()
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
